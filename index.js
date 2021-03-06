c24292505e81fd4600956f70e8b5bb1719305ef1
Marcelo Manzan <manzan@gmail.com>
2021-03-06 15:50:27 -0300

    express middleware :: body-parser (json payload)

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const consign = require('consign')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const port = 3000
 
 app.use(morgan('tiny'))
+app.use(bodyParser.json({type: ['application/*+json', 'application/json']}))
 
 consign()
     .include('./api')
