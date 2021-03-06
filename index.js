5db1d9b717d339a917315e6641d3380bdd2a2556
Marcelo Manzan <manzan@gmail.com>
2021-03-06 15:32:58 -0300

    express middleware :: morgan (http logs)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const consign = require('consign')
 const app = express()
+const morgan = require('morgan')
 const port = 3000
+
+app.use(morgan('tiny'))
 
 consign()
     .include('./api')
