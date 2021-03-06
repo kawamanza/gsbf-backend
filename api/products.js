cda6e2f4bbbec00d844589e594e43aae8df7b03c
Marcelo Manzan <manzan@gmail.com>
2021-03-06 15:23:20 -0300

    GET /products :: preparacao entrypoint base

--- api/products.js
+++ api/products.js
@@ -1,12 +1,33 @@
+const qs = require("querystring")
+
 module.exports = app => {
-    function entrypoint(req, res) {
-        res.json({
+
+    function resourceInfo(req) {
+        const query = (Object.keys(req.query).length ? "?" : "") + qs.stringify(req.query)
+        return ({
             title: "Products Entrypoint",
             _links: {
                 parent: {href: [req.protocol, "://", req.headers.host, '/'].join('')},
-                self:  {href: [req.protocol, "://", req.headers.host, '/products'].join('')},
+                self:  {href: [req.protocol, "://", req.headers.host, '/products', query].join('')},
             }
         })
+    }
+
+    async function list(req, res) {
+        const all = []
+        res.json({
+            ...resourceInfo(req),
+            title: "Products",
+            objects: all,
+        })
+    }
+
+    function entrypoint(req, res) {
+        if (Object.keys(req.query).length) {
+            list(req, res)
+        } else {
+            res.json(resourceInfo(req))
+        }
     }
     return { entrypoint }
 }
