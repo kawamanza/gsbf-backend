c36cff6c361f982701047b1094f92e60bb99185a
Marcelo Manzan <manzan@gmail.com>
2021-03-06 16:38:38 -0300

    POST /products :: criacao Produto

--- api/products.js
+++ api/products.js
@@ -1,6 +1,12 @@
 const qs = require("querystring")
 
 module.exports = app => {
+    const Product = app.mongoose.model('Product', {
+        title: {type: String, required: [true, 'Title required']},
+        price: {type: Number, required: [true, 'Price required'], min: [0.01, 'Price invalid']},
+        promo_price: {type: Number, min: [0.01, 'Promo Price invalid']},
+        currency: {type: String, required: [true, 'Currency required'], enum: ['BRL', 'USD', 'EUR', 'INR']},
+    }, 'products')
 
     function resourceInfo(req) {
         const query = (Object.keys(req.query).length ? "?" : "") + qs.stringify(req.query)
@@ -22,6 +28,19 @@ module.exports = app => {
         })
     }
 
+    async function create(req, res) {
+        const product = new Product(req.body)
+        try {
+            await product.validate()
+            await product.save()
+            res.status(201)
+            res.json({ok: true})
+        } catch(e) {
+            res.status(422)
+            res.json(e)
+        }
+    }
+
     function entrypoint(req, res) {
         if (Object.keys(req.query).length) {
             list(req, res)
@@ -29,5 +48,5 @@ module.exports = app => {
             res.json(resourceInfo(req))
         }
     }
-    return { entrypoint }
+    return { entrypoint, create }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 module.exports = app => {
     app.get('/', app.api.welcome.root)
     app.get('/products', app.api.products.entrypoint)
+    app.post('/products', app.api.products.create)
 }
