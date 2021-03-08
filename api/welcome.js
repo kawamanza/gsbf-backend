bf7566b442f569500ce64c133a554d3e29e96df9
Marcelo Manzan <manzan@gmail.com>
2021-03-08 00:54:41 -0300

    GET /currencies :: refactoring, codigo mais limpo

--- api/welcome.js
+++ api/welcome.js
@@ -16,28 +16,33 @@ module.exports = app => {
         })
     }
 
-    const hours_ago_in_millis = 60 * 60 * 1000
+    async function refreshCurrencies(token) {
+        const url = `http://api.currencylayer.com/live?access_key=${token}&format=1`
+        const result = await axios.get(url)
+        app.config.currencylayer = result.data
+        setTimeout(() => {
+            const currency_filepath = path.resolve(__dirname + '/../config/currencylayer.json')
+            fs.writeFileSync(currency_filepath, JSON.stringify(app.config.currencylayer, null, 2))
+        }, 500)
+    }
+
     async function currencies(req, res) {
-        let layer = app.config.currencylayer
-        const stale = (new Date() - layer.timestamp * 1000) > 8 * hours_ago_in_millis
-        if (stale && req.query.token) {
-            const token = req.query.token   // Token para teste: 'ab52e4a2a6ebd5462c96a1872402c3b1'
-            const url = `http://api.currencylayer.com/live?access_key=${token}&format=1`
-            const result = await axios.get(url)
-            app.config.currencylayer = layer = result.data
-            setTimeout(() => {
-                const currency_filepath = path.resolve(__dirname + '/../config/currencylayer.json')
-                fs.writeFileSync(currency_filepath, JSON.stringify(layer, null, 2))
-            }, 500)
+        const age = Math.floor((new Date() - app.config.currencylayer.timestamp * 1000) / 1000)  // seconds
+        const stale = age > 8 * 60 * 60
+        if (req.query.token) {
+            if (stale) {
+                const token = req.query.token   // Token para teste: 'ab52e4a2a6ebd5462c96a1872402c3b1'
+                await refreshCurrencies(token)
+            }
             res.status(302)
             res.setHeader('Location', linkUrl(req, '/currencies', {}))
             res.setHeader('Cache-Control', 'no-cache, no-store, private')
             return res.end()
         }
         res.json({
-            age: Math.floor((new Date() - layer.timestamp * 1000) / 1000),  // seconds
+            age,
             stale,
-            layer,
+            layer: app.config.currencylayer,
             _links: {
                 root: {href: linkUrl(req, '/', {})},
             }
