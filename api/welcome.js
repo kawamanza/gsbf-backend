4b3e4e47ab8fdc3feca97fa93708db20c3bab1be
Marcelo Manzan <manzan@gmail.com>
2021-03-08 00:39:22 -0300

    GET /currencies :: list or update conversions

--- api/welcome.js
+++ api/welcome.js
@@ -1,3 +1,7 @@
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+
 module.exports = app => {
     const { linkUrl } = app.config.utils
 
@@ -11,5 +15,34 @@ module.exports = app => {
             }
         })
     }
-    return { root }
+
+    const hours_ago_in_millis = 60 * 60 * 1000
+    async function currencies(req, res) {
+        let layer = app.config.currencylayer
+        const stale = (new Date() - layer.timestamp * 1000) > 8 * hours_ago_in_millis
+        if (stale && req.query.token) {
+            const token = req.query.token   // Token para teste: 'ab52e4a2a6ebd5462c96a1872402c3b1'
+            const url = `http://api.currencylayer.com/live?access_key=${token}&format=1`
+            const result = await axios.get(url)
+            app.config.currencylayer = layer = result.data
+            setTimeout(() => {
+                const currency_filepath = path.resolve(__dirname + '/../config/currencylayer.json')
+                fs.writeFileSync(currency_filepath, JSON.stringify(layer, null, 2))
+            }, 500)
+            res.status(302)
+            res.setHeader('Location', linkUrl(req, '/currencies', {}))
+            res.setHeader('Cache-Control', 'no-cache, no-store, private')
+            return res.end()
+        }
+        res.json({
+            age: Math.floor((new Date() - layer.timestamp * 1000) / 1000),  // seconds
+            stale,
+            layer,
+            _links: {
+                root: {href: linkUrl(req, '/', {})},
+            }
+        })
+    }
+
+    return { root, currencies }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     app.get('/', app.api.welcome.root)
     app.get('/products', app.api.products.entrypoint)
+    app.get('/currencies', app.api.welcome.currencies)
     app.get('/products/:id', app.api.products.detail)
     app.post('/products', app.api.products.create)
 }
