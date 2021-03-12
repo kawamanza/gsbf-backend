f3b50a47a97d4f5708aeecf8e251762bf7853316
Marcelo Manzan <manzan@gmail.com>
2021-03-12 06:35:46 -0300

    habilitando CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,9 @@
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
 module.exports = app => {
     app.use(morgan('tiny'))
     app.use(bodyParser.json({type: ['application/*+json', 'application/json']}))
+    app.use(cors())
 }
